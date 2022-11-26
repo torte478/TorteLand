cfdbda95d1b0163db80339f8d9a6823025159873
torte478 <torte478@gmail.com>
2022-11-26 11:44:18 +0300

    chr(Secret): prepare to release

--- dotnet/TorteLand/TorteLand.App/Program.cs
+++ dotnet/TorteLand/TorteLand.App/Program.cs
@@ -14,7 +14,6 @@ builder.Services.AddEndpointsApiExplorer();
 builder.Services.AddSwaggerGen();
 
 builder.Services
-       // .AddFileStorage(builder.Configuration)
        .AddFirebase(builder.Configuration)
        .AddCoreLogic();
 
